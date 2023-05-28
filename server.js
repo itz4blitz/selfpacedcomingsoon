b6a82c0a280791df3f41abc24ad5cec25c325313
Justin Scroggins <justinscroggins@outlook.com>
2023-05-28 00:41:16 -0400

    Add gitignore update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
